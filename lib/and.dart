ca0edcb227140aad19b050d99dacf480e543d734
mitrakov <mitrakov-artem@yandex.ru>
2023-03-07 14:02:13 +0400

    Use "and" indexes from model

--- lib/and.dart
+++ lib/and.dart
@@ -37,6 +37,8 @@ class _AndWidgetState extends State<AndWidget> {
 
   @override
   Widget build(BuildContext context) {
+    final j = widget.scenarioIdx;
+    final i = widget.idx;
     return ScopedModelDescendant<TestModel>(
       builder: (context, _, model) {
         _process(model);
@@ -48,14 +50,14 @@ class _AndWidgetState extends State<AndWidget> {
               Expanded(child: TextField(controller: actualCtrl, maxLines: 1024)),
               Expanded(child: Column(mainAxisSize: MainAxisSize.min, children: [
                 Row(mainAxisSize: MainAxisSize.min, children: [
-                  Expanded(child: TrixDropdown(hintText: "Process", options: const [stdout, stderr], getLabel: (s) => s, onChanged: (s) => model.setStdOutOrErr(widget.scenarioIdx, widget.idx, s==stdout))),
-                  Expanded(child: TrixDropdown(hintText: "As", options: const [csvText, json, xml, regex], getLabel: (s) => s, onChanged: (s) => model.setAs(widget.scenarioIdx, widget.idx, s)))
+                  Expanded(child: TrixDropdown(hintText: "Process", options: const [stdout, stderr], getLabel: (s) => s, onChanged: (s) => model.setStdOutOrErr(j, i, s==stdout))),
+                  Expanded(child: TrixDropdown(hintText: "As", options: const [csvText, json, xml, regex], getLabel: (s) => s, onChanged: (s) => model.setAs(j, i, s)))
                 ]),
-                TextFormField(initialValue: model.getQuery(widget.scenarioIdx, widget.idx), onChanged: (s) => model.setQuery(widget.scenarioIdx, widget.idx, s)),
+                TextFormField(initialValue: model.getQuery(j, i), onChanged: (s) => model.setQuery(j, i, s)),
                 Row(mainAxisSize: MainAxisSize.min, children: [
                   const Text("Should be"),
-                  Expanded(child: TrixDropdown(hintText: "Op", options: const [eq, gt, lt, ge, le], getLabel: (s) => s, onChanged: (s) => model.setOperation(widget.scenarioIdx, widget.idx, s))),
-                  Expanded(child: TextFormField(initialValue: model.getExpectedValue(widget.scenarioIdx, widget.idx), onChanged: (s) => model.setExpectedValue(widget.scenarioIdx, widget.idx, s))),
+                  Expanded(child: TrixDropdown(hintText: "Op", options: const [eq, gt, lt, ge, le], getLabel: (s) => s, onChanged: (s) => model.setOperation(j, i, s))),
+                  Expanded(child: TextFormField(initialValue: model.getExpectedValue(j, i), onChanged: (s) => model.setExpectedValue(j, i, s))),
                   _isOk(model) ? const Icon(Icons.done_outline, color: Colors.green) : const Icon(Icons.do_not_disturb, color: Colors.red),
                 ])
               ]))
@@ -67,11 +69,14 @@ class _AndWidgetState extends State<AndWidget> {
   }
 
   void _process(TestModel model) {
-    final s = model.getStdOutOrErr(widget.scenarioIdx, widget.idx) ? widget.stdoutCtrl.text.trim() : widget.stderrCtrl.text.trim();
-    final query = model.getQuery(widget.scenarioIdx, widget.idx);
+    final j = widget.scenarioIdx;
+    final i = widget.idx;
+    final s = model.getStdOutOrErr(j, i) ? widget.stdoutCtrl.text.trim() : widget.stderrCtrl.text.trim();
+    final query = model.getQuery(j, i);
     if (s.isEmpty || query.isEmpty) return;
+
     try {
-      switch (model.getAs(widget.scenarioIdx, widget.idx)) {
+      switch (model.getAs(j, i)) {
         case csvText:
           actualCtrl.text = TextProcessor.process(query, s);
           break;
@@ -92,9 +97,12 @@ class _AndWidgetState extends State<AndWidget> {
 
   bool _isOk(TestModel model) {
     final actual = actualCtrl.text.trim();
-    final expected = model.getExpectedValue(widget.scenarioIdx, widget.idx);
+    final j = widget.scenarioIdx;
+    final i = widget.idx;
+    final expected = model.getExpectedValue(j, i);
     if (expected.isEmpty) return false;
-    switch (model.getOp(widget.scenarioIdx, widget.idx)) {
+
+    switch (model.getOp(j, i)) {
       case eq: return actual == expected;
       case gt:
         final x = double.tryParse(actual);
