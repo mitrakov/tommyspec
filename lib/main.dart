f3534a18b54905ddc6b47c094cb2de1e65e9420a
mitrakov <mitrakov-artem@yandex.ru>
2023-03-10 15:39:44 +0400

    Add Open Model mechanism

--- lib/main.dart
+++ lib/main.dart
@@ -11,49 +11,51 @@ import 'package:tommyspec/scenario.dart';
 import 'package:tommyspec/utils/fnctrl.dart';
 
 void main() {
-  final model = TestModel();
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold(body: ScopedModel(model: model, child: MyApp()))));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: Scaffold(body: MyApp())));
 }
 
-class MyApp extends StatefulWidget {    // TODO: stateless?
+class MyApp extends StatefulWidget {
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   final runCtrl = FunctionController<String>();
+  TestModel _model = TestModel();
 
   @override
   Widget build(BuildContext context) {
-    return ScopedModelDescendant<TestModel>(
+    return ScopedModel<TestModel>(
+      model: _model,
+      child: ScopedModelDescendant<TestModel>(
         builder: (context, _, model) {
-        return PlatformMenuBar(
-          menus: [
-            PlatformMenu(label: "Hey-Hey", menus: [
-              PlatformMenuItem(label: "Quit", shortcut: const SingleActivator(LogicalKeyboardKey.keyQ, meta: true), onSelected: () => exit(0))
-            ]),
-            PlatformMenu(label: "File", menus: [
-              PlatformMenuItem(label: "Load", shortcut: SingleActivator(LogicalKeyboardKey.keyO, meta: true), onSelected: () => _openFile()),
-              PlatformMenuItem(label: "Save", shortcut: SingleActivator(LogicalKeyboardKey.keyS, meta: true), onSelected: () => _saveFile(model))
-            ])
-          ],
-          child: Shortcuts(
-            shortcuts: {
-              SingleActivator(LogicalKeyboardKey.enter): RunIntent() // move to menu?
-            },
-            child: Actions(
-              actions: {
-                RunIntent: CallbackAction(onInvoke: (_) => _run(model.command))
+          return PlatformMenuBar(
+            menus: [
+              PlatformMenu(label: "Hey-Hey", menus: [
+                PlatformMenuItem(label: "Quit", shortcut: const SingleActivator(LogicalKeyboardKey.keyQ, meta: true), onSelected: () => exit(0))
+              ]),
+              PlatformMenu(label: "File", menus: [
+                PlatformMenuItem(label: "Load", shortcut: SingleActivator(LogicalKeyboardKey.keyO, meta: true), onSelected: () => _openFile()),
+                PlatformMenuItem(label: "Save", shortcut: SingleActivator(LogicalKeyboardKey.keyS, meta: true), onSelected: () => _saveFile(model))
+              ])
+            ],
+            child: Shortcuts(
+              shortcuts: {
+                SingleActivator(LogicalKeyboardKey.enter): RunIntent() // move to menu?
               },
-              child: Column(mainAxisSize: MainAxisSize.min, crossAxisAlignment: CrossAxisAlignment.start, children: [
-                SizedBox(height: 50, child: Container(color: Colors.transparent, child: Row(children: [
-                  SizedBox(
-                      width: 300,
-                      child: TextFormField(initialValue: model.command, decoration: InputDecoration(hintText: "Command"), onChanged: (s) => model.command = s)
-                  ),
-                  OutlinedButton(child: Text("Run"), onPressed: () => _run(model.command))
-                ]))),
-                Expanded(child: ListView.builder(
+              child: Actions(
+                actions: {
+                  RunIntent: CallbackAction(onInvoke: (_) => _run(model.command))
+                },
+                child: Column(mainAxisSize: MainAxisSize.min, crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  SizedBox(height: 50, child: Container(color: Colors.transparent, child: Row(children: [
+                    SizedBox(
+                        width: 300,
+                        child: TextFormField(initialValue: model.command, decoration: InputDecoration(hintText: "Command"), onChanged: (s) => model.command = s)
+                    ),
+                    OutlinedButton(child: Text("Run"), onPressed: () => _run(model.command))
+                  ]))),
+                  Expanded(child: ListView.builder(
                     itemCount: model.scenariosCount + 1,
                     itemBuilder: (context, i) {
                       return i == model.scenariosCount
@@ -62,17 +64,18 @@ class _MyAppState extends State<MyApp> {
                       ],)
                           : ScenarioWidget(i, runCtrl);
                     }
-                ))
-              ])
+                  ))
+                ])
+              )
             )
-          )
-        );
-      }
+          );
+        }
+      )
     );
   }
 
   void _run(String command) {
-    setState(() { // we need this to update children
+    setState(() { // we need this to update children; TODO really?
       runCtrl.run(command);
     });
   }
@@ -88,11 +91,13 @@ class _MyAppState extends State<MyApp> {
   void _openFile() async {
     final result = await FilePicker.platform.pickFiles(dialogTitle: "Open file", type: FileType.custom, allowedExtensions: ["json"], withData: true, lockParentWindow: true);
     if (result != null) { // user may cancel
-      final bytes = result.files.first.bytes!; // may be null?
+      final bytes = result.files.first.bytes!;
       final str = Utf8Decoder().convert(bytes);
       final json = jsonDecode(str);
       final model = TestModel.fromJson(json);
-      print(model.command);
+      setState(() {
+        _model = model;
+      });
     }
   }
 }
