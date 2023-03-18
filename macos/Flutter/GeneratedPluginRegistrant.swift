5d1ff8bf37a4d9c40c066f6347fbe702af02ef6f
mitrakov <mitrakov-artem@yandex.ru>
2023-03-18 15:07:50 +0400

    Change external project name to CommandLineSpec

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import flutter_platform_alert
 import menubar
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FlutterPlatformAlertPlugin.register(with: registry.registrar(forPlugin: "FlutterPlatformAlertPlugin"))
   MenubarPlugin.register(with: registry.registrar(forPlugin: "MenubarPlugin"))
 }
